d1e318682e4b933a1949ca7ad5d769b7668ee591
Sam Kauffman <samuelkauffman@gmail.com>
2015-01-22 08:09:18 -0500

    added assert statements

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,5 @@
+console.log("does this work?")
+
 var assert = require ("assert");
 
 /**
@@ -5,33 +7,50 @@ var assert = require ("assert");
  * @param Number B
  * @return sum of A and B
  */
-function add(A, B){
-    return (A + B);
+
+function add(A,B){
+    return A+B;
+    console.log(A+B);
 }
+
+    assert(add(2,6) == 12);
 
 /**
  * @param Number A
  * @param Number B
  * @return difference of A and B
  */
-function diff(A, B){
-    return (A - B);
+
+function diff(A,B){
+    return A-B;
 }
+
+    assert(diff(10,2) == 10)
 
 /**
  * @param Number A
  * @param Number B
  * @return product of A times B
  */
-function prod(A, B){
-    return (A * B);
+
+function prod(A,B){
+    return A*B;
 }
 
+    assert(prod(4,2) == 8)
+    
 /**
  * @param Number A
  * @param Number B
  * @return division of A by B
  */
-function div(A, B){
-    return (A / B);
+
+function div(A,B){
+    return A / B;
 }
+
+    assert(div(6,3) == 2)
+
+
+    
+assert(plus("one", "one") === 2);
