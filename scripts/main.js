d0e4ff6f8657ea968d31f074cc62bda73023e121
Sam Kauffman <samuelkauffman@gmail.com>
2015-01-22 00:32:55 -0500

    Added 4 functions to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,37 @@
-console.log("hello");
+var assert = require ("assert");
+
+/**
+ * @param Number A
+ * @param Number B
+ * @return sum of A and B
+ */
+function add(A, B){
+    return (A + B);
+}
+
+/**
+ * @param Number A
+ * @param Number B
+ * @return difference of A and B
+ */
+function diff(A, B){
+    return (A - B);
+}
+
+/**
+ * @param Number A
+ * @param Number B
+ * @return product of A times B
+ */
+function prod(A, B){
+    return (A * B);
+}
+
+/**
+ * @param Number A
+ * @param Number B
+ * @return division of A by B
+ */
+function div(A, B){
+    return (A / B);
+}
