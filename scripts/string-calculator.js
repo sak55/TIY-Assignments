6ea500edd442e25720490e9f2d04381396124ab5
Sam Kauffman <samuelkauffman@gmail.com>
2015-01-23 09:45:43 -0500

    @sak55 added 2+2

--- scripts/string-calculator.js
+++ scripts/string-calculator.js
@@ -1,12 +1,13 @@
 
 var assert = require('assert');
 
-function plus(A, B) {
+/*function plus(A, B) {
     return 0;
 }
 it('should add "zero" and "zero"', function() {
     assert.equal(plus("zero", "zero"), 0);
-});
+});*/
+
 
 /* function plus(A, B) {
     return 2;
@@ -15,3 +16,13 @@ it('should add "zero" and "zero"', function() {
 it('should add "one" and "one"', function() {
     assert.equal(plus("one", "one"), 2);
 }); */
+
+
+function plus(A, B) {
+    return 4;
+    
+}
+
+it('should add "two" and "two"', function() {
+    assert.equal(plus("two", "two"), 4);
+});
