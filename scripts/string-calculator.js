66bafb6e56c6af3cb292fa4147c2ed2769117e6d
Sam Kauffman <samuelkauffman@gmail.com>
2015-01-23 09:38:25 -0500

    @taylorfoster90

--- scripts/string-calculator.js
+++ scripts/string-calculator.js
@@ -2,9 +2,16 @@
 var assert = require('assert');
 
 function plus(A, B) {
+    return 0;
+}
+it('should add "zero" and "zero"', function() {
+    assert.equal(plus("zero", "zero"), 0);
+});
+
+/* function plus(A, B) {
     return 2;
 }
 
 it('should add "one" and "one"', function() {
     assert.equal(plus("one", "one"), 2);
-});
+}); */
