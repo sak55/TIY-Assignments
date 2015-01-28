da088f72d8bb7414131fab50ebc681d5e1224eb7
Sam Kauffman <samuelkauffman@gmail.com>
2015-01-27 21:19:48 -0500

    deleted commented out code

--- scripts/gymnastics.js
+++ scripts/gymnastics.js
@@ -45,34 +45,3 @@ var theAnswer = answer();
         assert(theAnswer.PushEvent);
         assert(theAnswer.PushEvent.total);
     });
-
-//function answer(){
-//  return {
-//  'total': events.length,
-//  };
-//} // END answer
-//
-//console.log(answer());
-//
-//describe('the setup', function(){
-//    it('should have events', function(){
-//        assert(events);
-//    });
-//    it('should return an "answer" events', function(){
-//        assert(answer);
-//        assert(typeof answer == 'function');
-//    });
-//});
-//
-//describe('the answer', function(){
-//    var theAnswer = answer();
-//    
-//    it ('should have 30 total events', function(){
-//        assert(theAnswer.total === 30);
-//    });
-//    
-//    
-//    it('should have some "PushEvent" entries', function(){
-//        assert(theAnswer.PushEvents.total);
-//    });
-//});
