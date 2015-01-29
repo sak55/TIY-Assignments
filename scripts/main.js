c8642d6f944f48c1f5aabcf7f29aeb375ee54711
Sam Kauffman <samuelkauffman@gmail.com>
2015-01-29 06:45:11 -0500

    prints html in loop

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,44 @@
 function print(board){
   // Before any rows are printed, print the `<table>`...
     
-    console.log('<table class="chessboard"><tbody>');
-    console.log('<tr><th></th><th>a</th><th>b</th><th>c</th><th>d</th><th>e</th><th>f</th><th>g</th><th>h</th></tr>');
-    
+    console.log('<table>class="chessboard"><tbody><tr><th></th><th>a</th><th>b</th><th>c</th><th>d</th><th>e</th><th>f</th><th>g</th><th>h</th></tr>');
     for (counter = 8; counter > 0; counter--) {
-        console.log('<tr><th>');
-        console.log(counter);
-        if (counter % 2) {
-            console.log('</th><td class="light"></td><td class="dark"></td><td class="light"></td><td class="dark"></td><td class="light"></td><td class="dark"></td><td class="light"></td><td class="dark">')};
-        else { 
-            console.log('</th><td class="dark"></td><td class="light"></td><td class="dark"></td><td class="light"></td><td class="dark"></td><td class="light"></td><td class="dark"></td><td class="light">')};
-    console.log('</td></tr></tbody></table>');};
+        var row = ['<td class=',color1,'></td><td class=',color2,'></td><td class=',color1,'></td><td class=',color2,'></td><td class=',color1,'></td><td class=',color2,'></td><td class=',color2,'></td><td class=',color2,'>'], 
+            color1, 
+            color2;
+        console.log('<tr><th>', counter, '</th>');
+        if (counter % 2 === 0) {
+            color1 = "light";
+            color2 = "dark";
+            console.log(row);}
+        else {
+            color1 = "dark";
+            color2 = "light";
+            console.log(row);}
+        console.log('</td></tr></tbody></table>');};
         counter--;    
     };
+
+print();
+
+// _.forEach(board, function(row, rank){
+//
+//      // Before any cells are printed...
+//
+//      _.forEach(row, function(square, file){
+//
+//          // In here, we have `rank`, `file`, and `square`...
+//
+//      }); // END foreach(row)
+//
+//      // After all the cells are printed...
+//
+//  }); // END foreach(board)
+//
+//  // After all the rows are printed...
+//  console.log('</table> <!-- .chessboard -->');
+//} // END print
+
     
 //    console.log('<table class="chessboard">');
 //            <tbody>
@@ -119,24 +144,7 @@ function print(board){
 //            </tbody>
 //        </table>');
                     
-  _.forEach(board, function(row, rank){
-
-      // Before any cells are printed...
-
-      _.forEach(row, function(square, file){
-
-          // In here, we have `rank`, `file`, and `square`...
-
-      }); // END foreach(row)
-
-      // After all the cells are printed...
-
-  }); // END foreach(board)
-
-  // After all the rows are printed...
-  console.log('</table> <!-- .chessboard -->');
-} // END print
-
+ 
 //
 //    var board = [
 //        ['0','1','2','3','4','5','6','7','8'],
