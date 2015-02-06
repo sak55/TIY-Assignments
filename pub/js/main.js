15dcd6d40f46f1ff9047d4c0586635b21cf29489
Sam Kauffman <samuelkauffman@gmail.com>
2015-02-06 14:49:25 -0500

    added click button function

--- pub/js/main.js
+++ pub/js/main.js
@@ -5,3 +5,9 @@
 //        $('#publicactivity a:last').hide();
 //    }
 //}
+var mycomment;
+
+$("postme").click(function(mycomment){
+    $.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/257/comments?access_token=46045169c764de2548ed9b3bd28b3bc88c58daf4", JSON.stringify({"body","mycomment"})
+          );
+});
