20789e69d97ea9a125847232df2066cd4ae1ce04
Sam Kauffman <samuelkauffman@gmail.com>
2015-02-06 16:34:58 -0500

    button works most of the way just need to clear text box after click

--- pub/js/main.js
+++ pub/js/main.js
@@ -5,9 +5,13 @@
 //        $('#publicactivity a:last').hide();
 //    }
 //}
-var mycomment;
 
-$("postme").click(function(mycomment){
-    $.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/257/comments?access_token=46045169c764de2548ed9b3bd28b3bc88c58daf4", JSON.stringify({"body","mycomment"})
-          );
+$(".postme").click(function submitme() {
+    $.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/257/comments?access_token=46045169c764de2548ed9b3bd28b3bc88c58daf4", JSON.stringify({"body": $("#mytextarea").val()})
+        );
 });
+
+//function submitme(mycomment) { 
+//    stringify(mycomment);
+//$.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/257/comments?access_token=46045169c764de2548ed9b3bd28b3bc88c58daf4", JSON.stringify({"body": mycomment})
+//        );}
