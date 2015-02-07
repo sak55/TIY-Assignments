40e70725ab6043886779269a302d0b7a31b4efb1
Sam Kauffman <samuelkauffman@gmail.com>
2015-02-07 10:26:46 -0500

    fixed tab withs and made desktop set

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,17 +1,13 @@
-//var vp = $(window).width();
-//
-//function hidetab() {
-//    if (vp < 992) {
-//        $('#publicactivity a:last').hide();
-//    }
-//}
+$(document).ready(function () {
 
-$(".postme").click(function submitme() {
-    $.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/257/comments?access_token=46045169c764de2548ed9b3bd28b3bc88c58daf4", JSON.stringify({"body": $("#mytextarea").val()})
-        );
-});
 
-//function submitme(mycomment) { 
-//    stringify(mycomment);
-//$.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/257/comments?access_token=46045169c764de2548ed9b3bd28b3bc88c58daf4", JSON.stringify({"body": mycomment})
-//        );}
+
+//This function posts a comment to my WIP Issue and then clears the comment box.
+    $(".postme").click(function submitme() {
+        $.post("https://api.github.com/repos/TheIronYard--Orlando/FEE--2015--SPRING/issues/257/comments?access_token=46045169c764de2548ed9b3bd28b3bc88c58daf4", JSON.stringify({"body": $("#mytextarea").val()}),
+               $("#mytextarea").val(""));
+    });
+
+    
+
+}) //jQuery document.ready function closing bracket
