76aa8fcbd73e272654d034226ba52d393851b134
Sam Kauffman <samuelkauffman@gmail.com>
2015-02-10 10:38:03 -0500

    flops time and code

--- pub/Colour-Clock/js/main.js
+++ pub/Colour-Clock/js/main.js
@@ -13,32 +13,37 @@
 //    
 //});
 //
-var d, h, m, s, colorTime;
+var d = new Date(), h, m, s, colorTime;
 ////sorry moment.js, maybe next time
 
 function showTime() {
-   d = new Date(); //Date is a JS function be sure to use a capital D
+    document.getElementById("thecolor").innerHTML =  "";
+    document.getElementById("theclock").innerHTML =  "";
+   d.setSeconds(d.getSeconds() + 1); //Date is a JS function be sure to use a capital D
+    
    h = d.getHours(); //returns just the Hours
    m = d.getMinutes();//returns just the 
    s = d.getSeconds();//returns just the 
 
-//  need more zero to keep six digits!
-   if(h <= 9) h = '0' + h;
-   if(m <= 9) m = '0' + m;
-   if(s <= 9) s = '0' + s;
+    //  need more zero to keep six digits!
+       if(h <= 9) h = '0' + h;
+       if(m <= 9) m = '0' + m;
+       if(s <= 9) s = '0' + s;
 
-    //provides the hexidecimal code for the color
-   colorTime = "#" + h + m + s;
+        //provides the hexidecimal code for the color
+       colorTime = "#" + h + m + s;
 
-    //colors the background to the per second color
-   document.body.style.background = colorTime;
-
-    //writes to document the color
-    document.getElementByclass("thecolor").innerHTML =  colorTime;
-    //writes to document the time
-   document.getElementByclass("theclock").innerHTML =  (h + ":" + m + ":" + s);
-//sets the interval to 1 second
-   setInterval(showTime, 1000);
+        //colors the background to the per second color
+       document.body.style.background = colorTime;
+        //writes to document the color
+        if (s % 2 == 0) {
+            document.getElementById("thecolor").innerHTML =  colorTime;}
+        //writes to document the time
+       else {
+            document.getElementById("theclock").innerHTML =  (h + ":" + m + ":" + s);}
+    //sets the interval to 1 second
+setTimeout(showTime, 1000);       
+    
 }
 
 showTime();
