e0a9e8749d903f83e9c6dacf95222652b595b59d
Sam Kauffman <samuelkauffman@gmail.com>
2015-02-10 16:59:19 -0500

    first assert works

--- scripts/pieces.js
+++ scripts/pieces.js
@@ -1,48 +1,82 @@
+
+var assert = require("assert");
 /**
  * @constructor Piece
  * @param String color of Piece: "white" or "black"
  * @param String name of Piece, e.g. "queen", "rook", "knight"
  * @param String position of Piece, e.g. "a7", "d8"
  */
-function Piece(color, name, position){
+function Piece(color, name, position) {
   // Constructor
     this.color = color;
     this.name = name;
     this.position = position;
 }
 
-Piece.prototype = {
-        getFullName = {
-            return this.color + " " + this.name;
-        }
-};
-// END Piece.prototype
+it("should have Piece", function(){
+    assert(Piece);
+});
 
 /** @property String _color of Piece passed to Constructor
  * @property String _name of Piece passed to Constructor
  * @property String _position of Piece passed to Constructor
  */
- 
- /** @method position
+
+//Piece.prototype = {
+//    getName: function (){
+//        return this.name;
+//        getFullName = {
+//            return this.color + " " + this.name;
+//        }
+//    }
+//};
+//tests that we have a function piece
+
+/** @method position
  * @returns String current position, e.g. "a8", "f3"
  */
-
- /** @method color
+Piece.prototype = {
+    getPosition: function() {
+        return this.position;
+    }
+};
+/** @method color
  * @returns String color of Piece: "white" or "black"
  */
-
- /** @method name
+Piece.prototype = {
+    getColor: function() {
+        return this.color;
+    }
+};
+/** @method name
  * @returns String name of Piece, e.g. "queen", "rook", "knight"
  */
 
- /** @method abbrev
+Piece.prototype = {
+    getName: function() {
+        return this.name;
+    }
+};
+
+
+/** @method abbrev
  * @returns String abbreviation of Piece, e.g. "white queen" == 'q'
  */
 
+Piece.prototype = {
+    getAbbrev: function() {
+        return this.name.substring(0,1)
+    }
+}
  /** @method toString()
  * @return String for printing / debugging, e.g. "black queen d8"
  */
 
+Piece.prototype = {
+    geToHTML: function() {
+        return this.color + " " + this.name + " " + this.position
+    }
+};
  /** @method toHTML()
  * @return String HTML entity, e.g. "black knight" == "&#x265e"
  */
