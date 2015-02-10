7d7bd7b53c00c4bec9dd0413f00fbdfd3b3324d9
Sam Kauffman <samuelkauffman@gmail.com>
2015-02-10 16:14:15 -0500

    started creating constructors and methods

--- pub/js/main.js
+++ pub/js/main.js
@@ -1,1 +1,4 @@
+//build out and test the Constructors and Prototypes. Then refactor your chessboard to use those instead of the literals and loose functions.
+//
+//Your Game should have an internal list of moves, an Array of Function items that advance the state of the Board when invoked. Start with the original move list but expand it to include this slightly gory mid-game, which has captures and castling.
 
--- scripts/pieces.js
+++ scripts/pieces.js
@@ -3,35 +3,49 @@
  * @param String color of Piece: "white" or "black"
  * @param String name of Piece, e.g. "queen", "rook", "knight"
  * @param String position of Piece, e.g. "a7", "d8"
- *
- * @property String _color of Piece passed to Constructor
- * @property String _name of Piece passed to Constructor
- * @property String _position of Piece passed to Constructor
- *
- * @method position
- * @returns String current position, e.g. "a8", "f3"
- *
- * @method color
- * @returns String color of Piece: "white" or "black"
- *
- * @method name
- * @returns String name of Piece, e.g. "queen", "rook", "knight"
- *
- * @method abbrev
- * @returns String abbreviation of Piece, e.g. "white queen" == 'q'
- *
- * @method toString()
- * @return String for printing / debugging, e.g. "black queen d8"
- *
- * @method toHTML()
- * @return String HTML entity, e.g. "black knight" == "&#x265e"
- *
- * @method toUTF8()
- * @return String UTF8 character, e.g. "black knight" == "\u265e"
  */
 function Piece(color, name, position){
   // Constructor
+    this.color = color;
+    this.name = name;
+    this.position = position;
 }
 
 Piece.prototype = {
-} // END Piece.prototype
+        getFullName = {
+            return this.color + " " + this.name;
+        }
+};
+// END Piece.prototype
+
+/** @property String _color of Piece passed to Constructor
+ * @property String _name of Piece passed to Constructor
+ * @property String _position of Piece passed to Constructor
+ */
+ 
+ /** @method position
+ * @returns String current position, e.g. "a8", "f3"
+ */
+
+ /** @method color
+ * @returns String color of Piece: "white" or "black"
+ */
+
+ /** @method name
+ * @returns String name of Piece, e.g. "queen", "rook", "knight"
+ */
+
+ /** @method abbrev
+ * @returns String abbreviation of Piece, e.g. "white queen" == 'q'
+ */
+
+ /** @method toString()
+ * @return String for printing / debugging, e.g. "black queen d8"
+ */
+
+ /** @method toHTML()
+ * @return String HTML entity, e.g. "black knight" == "&#x265e"
+ */
+ /* @method toUTF8()
+ * @return String UTF8 character, e.g. "black knight" == "\u265e"
+ */
