3ef2fbaad1ac27f060abcf91a3df82122b5b22d6
Sam Kauffman <samuelkauffman@gmail.com>
2015-02-15 14:43:45 -0500

    finished string calc

--- katas/string-calculator.js
+++ katas/string-calculator.js
@@ -8,67 +8,64 @@ var _ = require('lodash');
  * @param String B an English number word
  * @returns Number representing A + B
  */
-var dictNum = {0 : 'zero',
-               1 : 'one',
-               2 : 'two',
-               3 : 'three',
-               4 : 'four',
-               5 : 'five',
-               6 : 'six',
-               7 : 'seven',
-               8: 'eight',
-               9 : 'nine',
-               10 : 'ten',
-               11 : 'eleven',
-               12 : 'twelve',
-               13 : 'thirteen',
-               14 : 'forteen',
-               15 : 'fifteen',
-               16 : 'sixteen',
-               17 : 'seventeen',
-               18 : 'eighteen',
-               19 : 'ninteen',
-               20 : 'twenty'};
 
-function plus(A, B) {
-    var n, key, numA, numB, answer;
-    _.forEach(dictNum, function (n, key) {
-        if (n === A) {
-            return numA = key;
-        }
-        if (n === B) {
-            return numB = key;
-        }
-    });
-    answer = parseInt(numA) + parseInt(numB);
+
+var dictNum = {'zero' : 0,
+               'one' : 1,
+               'two' : 2,
+               'three' : 3,
+               'four' : 4,
+               'five' : 5,
+               'six' : 6,
+               'seven' : 7,
+               'eight' : 8,
+               'nine' : 9,
+               'ten' : 10,
+               'eleven' : 11,
+               'twelve' : 12,
+               'thirteen' : 13,
+               'forteen' : 14,
+               'fifteen' : 15,
+               'sixteen' : 16,
+               'seventeen' : 17,
+               'eighteen' : 18,
+               'ninteen' : 19,
+               'twenty' : 20,
+               'thirty' :30 ,
+               'forty' : 40,
+               'fifty' : 50,
+               'sixty' : 60,
+               'seventy' : 70,
+               'eighty' : 80,
+               'ninty' : 90,
+              'one hundred' : 100};
+
+function addme(A, B) {
+    var answer;
+    answer = toNumber(A) + toNumber(B)
     console.log(answer);
 }
  
-plus('two', 'three');
-    
-//   
-//plus("four", "two");
-//
-//it('should return number for the string', function() {
-//   assert.equal(num('five'),5); 
-//});
-//        
-//it('should return two strings added as numbers after looking at the dictionary', function() {
-//    assert.equal(plus('zero', 'ten'),10);
-//});
-
 /**
  * @param String A an English number word
  * @param String B an English number word
  * @returns Number representing A - B
  */
-//function minus(A, B){
-//}
+function minus(A, B) {
+    var answer;
+    answer = toNumber(A) - toNumber(B)
+    console.log(answer);
+}
 
 /**
  * @param String word an English number word
  * @returns Number translation of `word`
  */
-//function toNumber(word){
-//}
+function toNumber(word){
+    return dictNum[word];
+}
     
+addme('zero', 'two');
+addme('fifty', 'five');
+minus ('three', 'two');
+minus('fifty', 'five');
