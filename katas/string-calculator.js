5ec08d8ee953dace8b33ed12c5b4f662be24e500
Sam Kauffman <samuelkauffman@gmail.com>
2015-02-15 12:03:36 -0500

    string calc working

--- katas/string-calculator.js
+++ katas/string-calculator.js
@@ -1,22 +1,61 @@
+var assert = require('assert');
+var _ = require('lodash');
+
+    
+
 /**
  * @param String A an English number word
  * @param String B an English number word
  * @returns Number representing A + B
  */
-var stringNum = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+var dictNum = {0 : 'zero',
+               1 : 'one',
+               2 : 'two',
+               3 : 'three',
+               4 : 'four',
+               5 : 'five',
+               6 : 'six',
+               7 : 'seven',
+               8: 'eight',
+               9 : 'nine',
+               10 : 'ten',
+               11 : 'eleven',
+               12 : 'twelve',
+               13 : 'thirteen',
+               14 : 'forteen',
+               15 : 'fifteen',
+               16 : 'sixteen',
+               17 : 'seventeen',
+               18 : 'eighteen',
+               19 : 'ninteen',
+               20 : 'twenty'};
 
 function plus(A, B) {
-    var i;
-    for (i = 0; i < stringNum.length; i++) {
-        if (A = stringNum[i]) {
-            A = i;
+    var n, key, numA, numB, answer;
+    _.forEach(dictNum, function (n, key) {
+        if (n === A) {
+            return numA = key;
         }
-        if (B = stringNum[i]) {
-            B = i;
+        if (n === B) {
+            return numB = key;
         }
-    }
-    return (A + B);
-} // END plus
+    });
+    answer = parseInt(numA) + parseInt(numB);
+    console.log(answer);
+}
+ 
+plus('two', 'three');
+    
+//   
+//plus("four", "two");
+//
+//it('should return number for the string', function() {
+//   assert.equal(num('five'),5); 
+//});
+//        
+//it('should return two strings added as numbers after looking at the dictionary', function() {
+//    assert.equal(plus('zero', 'ten'),10);
+//});
 
 /**
  * @param String A an English number word
@@ -32,3 +71,4 @@ function plus(A, B) {
  */
 //function toNumber(word){
 //}
+    
